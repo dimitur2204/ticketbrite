0ecbb81d036cba870d4b6244b5f209d430eb5091
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-06-14 22:29:16 +0300

    Added user creation and password hashing logic

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // An interface that describes the prop to create a new user
 interface UserAttrs {
@@ -27,6 +28,15 @@ const userSchema = new mongoose.Schema({
 		required: true,
 	},
 });
+
+userSchema.pre('save', async function (done) {
+	if (this.isModified('password')) {
+		const hashed = await Password.toHash(this.get('password'));
+		this.set('password', hashed);
+	}
+	done();
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
 
 const User = mongoose.model<UserDoc, UserModel>('user', userSchema);

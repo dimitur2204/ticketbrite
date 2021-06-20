ab6493c9bb93c3c90659e9b2e2e843b26a0a1e67
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-06-20 21:27:54 +0300

    Added tests for the whole auth service

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,7 +1,15 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 
+declare global {
+	namespace NodeJS {
+		interface Global {
+			signin(): Promise<string[]>;
+		}
+	}
+}
 let mongo: any;
 beforeAll(async () => {
 	process.env.JWT_KEY = 'asdf';
@@ -25,3 +33,17 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const res = await request(app)
+		.post('/api/users/signup')
+		.send({ email, password })
+		.expect(201);
+
+	const cookie = res.get('Set-Cookie');
+
+	return cookie;
+};

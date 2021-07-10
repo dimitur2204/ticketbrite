25987720db11bd1f862853deecfe0612a315eef3
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-07-10 14:06:06 +0300

    Added tickets service infra files and setup

---
+++ tickets/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const init = async () => {
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT_KEY must be defined');
+	}
+
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Connected to db');
+	} catch (error) {
+		console.error(error);
+	}
+	app.listen(3000, () => {
+		console.log('Listening on port 3000!');
+	});
+};
+
+init();
